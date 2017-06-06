adad7e59a952cd569fa0901977f0c47b872add2c
Abhinav Baid <abhinavbaid@gmail.com>
2017-06-06 21:58:56 +0530

    Add Chapter 7 solutions

---
+++ C07/Fourinone.cpp
@@ -0,0 +1,63 @@
+/*
+ * Ex 7.4
+ * Fourinone.cpp
+ */
+
+
+class Four
+{
+  public:
+    void f();
+    void f(int);
+    void f(int, int);
+    void f(int, int, int);
+};
+
+void
+Four::f()
+{
+}
+
+void
+Four::f(int)
+{
+}
+
+void
+Four::f(int, int)
+{
+}
+
+void
+Four::f(int, int, int)
+{
+}
+
+class One
+{
+  public:
+    void f(int = 0, int = 0, int = 0);
+};
+
+void
+One::f(int, int, int)
+{
+}
+
+int
+main(int argc, char **argv)
+{
+    // old style
+    Four four;
+    four.f();
+    four.f(1);
+    four.f(1, 2);
+    four.f(1, 2, 3);
+    // new style
+    One one;
+    one.f();
+    one.f(1);
+    one.f(1, 2);
+    one.f(1, 2, 3);
+    return 0;
+}
---
+++ C07/Text.cpp
@@ -0,0 +1,49 @@
+/*
+ * Ex 7.1
+ * Text.cpp
+ */
+
+
+#include <iostream>
+#include <fstream>
+#include "../require.h"
+using namespace std;
+
+class Text
+{
+    string text;
+  public:
+      Text();
+      Text(string);
+    string contents();
+};
+
+Text::Text()
+{
+}
+
+Text::Text(string filename)
+{
+    ifstream in(filename);
+    string line;
+    while (getline(in, line))
+    {
+        text += text.empty()? line : '\n' + line;
+    }
+}
+
+string
+Text::contents()
+{
+    return text;
+}
+
+int
+main(int argc, char **argv)
+{
+    requireArgs(argc, 1);
+    string filename(argv[1]);
+    Text text(filename);
+    cout << text.contents() << endl;
+    return 0;
+}
