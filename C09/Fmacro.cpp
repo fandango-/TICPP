c7a329178a4c0594081154d558115935f5044bf1
Abhinav Baid <abhinavbaid@gmail.com>
2017-06-07 12:31:11 +0530

    Add Chapter 9 solutions

---
+++ C09/Fmacro.cpp
@@ -0,0 +1,20 @@
+/*
+ * Ex 9.1
+ * Fmacro.cpp
+ */
+
+
+#include <iostream>
+using namespace std;
+
+#define F(x) (x + 1)            // wrong
+#define F1(x) ((x) + 1)         // correct
+
+int
+main(int argc, char **argv)
+{
+    int a = 0x0f, b = 0x07;
+    cout << F(a >= b ? 0 : 1) << endl;
+    cout << F1(a >= b ? 0 : 1) << endl;
+    return 0;
+}
