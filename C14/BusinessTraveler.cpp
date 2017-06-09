7448d6fd76b3ba40ad2fc59b0107105cbb8b0cb1
Abhinav Baid <abhinavbaid@gmail.com>
2017-06-09 14:16:44 +0530

    Add Chapter 14 solutions

---
+++ C14/BusinessTraveler.cpp
@@ -0,0 +1,89 @@
+/*
+ * Ex 14.14
+ * BusinessTraveler.cpp
+ */
+
+
+#include <iostream>
+using namespace std;
+
+class Traveler
+{
+    string str;
+  public:
+    Traveler(string s):str(s)
+    {
+    }
+    Traveler(const Traveler & t):str(t.str)
+    {
+    }
+    Traveler & operator=(const Traveler & t)
+    {
+        str = t.str;
+        return *this;
+    }
+    friend ostream & operator<<(ostream & os, const Traveler & t)
+    {
+        return os << "Traveler: " << t.str;
+    }
+};
+
+class Pager
+{
+    string str;
+  public:
+    Pager(string s):str(s)
+    {
+    }
+    Pager(const Pager & p):str(p.str)
+    {
+    }
+    Pager & operator=(const Pager & p)
+    {
+        str = p.str;
+        return *this;
+    }
+    friend ostream & operator<<(ostream & os, const Pager & p)
+    {
+        return os << "Pager: " << p.str;
+    }
+};
+
+class BusinessTraveler:public Traveler
+{
+    Pager p;
+  public:
+    BusinessTraveler():Traveler(""), p("")
+    {
+    }
+    BusinessTraveler(string s):Traveler(s), p(s)
+    {
+    }
+    BusinessTraveler(const BusinessTraveler & bt):Traveler(bt), p(bt.p)
+    {
+    }
+    BusinessTraveler & operator=(const BusinessTraveler & bt)
+    {
+        Traveler::operator=(bt);
+        p = bt.p;
+        return *this;
+    }
+    friend ostream & operator<<(ostream & os, const BusinessTraveler & bt)
+    {
+        return os << "BusinessTraveler: " << endl << (Traveler &) bt << endl <<
+            bt.p;
+    }
+};
+
+int
+main(int argc, char **argv)
+{
+    BusinessTraveler bt, bt2("xyz");
+    cout << "default" << endl << bt << endl;
+    cout << "one argument" << endl << bt2 << endl;
+    BusinessTraveler bt3 = bt2;
+    cout << "copy constructor" << endl << bt3 << endl;
+    bt2 = bt;
+    cout << "operator=" << endl << bt2 << endl;
+    return 0;
+}
---
+++ C14/Threelevel.cpp
@@ -0,0 +1,55 @@
+/*
+ * Ex 14.3
+ * Threelevel.cpp
+ */
+
+
+#include <iostream>
+using namespace std;
+
+class A
+{
+  public:
+    A()
+    {
+        cout << "Constructor for A called" << endl;
+    }
+     ~A()
+    {
+        cout << "Destructor for A called" << endl;
+    }
+};
+
+class B:public A
+{
+  public:
+    B()
+    {
+        cout << "Constructor for B called" << endl;
+    }
+     ~B()
+    {
+        cout << "Destructor for B called" << endl;
+    }
+};
+
+class C:public B
+{
+  public:
+    C()
+    {
+        cout << "Constructor for C called" << endl;
+    }
+     ~C()
+    {
+        cout << "Destructor for C called" << endl;
+    }
+};
+
+int
+main(int argc, char **argv)
+{
+    C c;
+    // Order of calls is A, B, C for constructors and the reverse for destructors
+    return 0;
+}
